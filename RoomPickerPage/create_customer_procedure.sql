20c4357512929e8ff1adf0fee33c6f55b66f6191
Colin Gillette <cgillette@mfncuso.net>
2019-05-03 08:57:03 -0500

    add procedure to create new customers

---
+++ RoomPickerPage/create_customer_procedure.sql
@@ -0,0 +1,12 @@
+-- At work. Currently untested
+
+create or replace procedure create_customer
+    (first_in customers.first%type,
+    last_in customers.last%type,
+    card_no_in customers.card_no%type,
+    card_type_in customers.card_type%type) is
+begin
+    insert into customers (customer_id, first, last, card_no, card_type)
+        values (customer_id_seq.nextval, first_in, last_in, card_no_in, card_type_in);
+    commit;
+end;
