3ef31de1dfee24e1103ac06372ebb53d60abe029
Colin Gillette <cgillette@mfncuso.net>
2019-05-03 08:39:20 -0500

    check to see if this is a new customer, working on logic if it is

--- RoomPickerPage/room_picker_procedure.sql
+++ RoomPickerPage/room_picker_procedure.sql
@@ -1,3 +1,5 @@
+-- At work. Changes currently untested
+
 create or replace procedure room_picker
     (first_name_in customers.first%type,
     last_name_in customers.last%type,
@@ -17,7 +19,16 @@ create or replace procedure room_picker
             and l.city = city_in;
     room_row room_cursor%rowtype;
     room_good boolean;
+    customer_is_new boolean;
 begin
+    customer_is_new := check_for_customer(first_name_in, last_name_in, card_number_in, card_company_name_in);
+
+    if customer_is_new = true then
+
+    else
+
+    end if;
+    
     for room_row in room_cursor
     loop
         room_good := true;
