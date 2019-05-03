44a3c7a90733bba9c99c23a0cdef29e6ef0fe535
Colin Gillette <cgillette@mfncuso.net>
2019-05-03 12:48:10 -0500

    add toolkit for reservation_list

---
+++ ReservationListPage/reservation_list_toolkit.sql
@@ -0,0 +1,55 @@
+-- At work. Currently untested
+
+create or replace procedure show_current_reservations as
+    cursor location_cursor is
+    select location_id, city, state_province, country from locations;
+    location_row location_cursor%rowtype;
+begin
+    htp.print('<!DOCTYPE html PUBLIC"-//W#C//DTD HTML 4.01 Transitional//EN">
+    <html>
+        <head>
+            <title>Reservation List</title>
+            <link rel="stylesheet" href="index.css">
+            <a href="createReservation.html" id="createreservation">Create Reservation</a>
+            <a href="reservationList.html">Administration</a>
+        </head>
+        <hr>
+        <body>
+            <h1>Current Reservations</h1>
+
+            <form name="reservation-list-form" action="edit_reservation">');
+                for location_row in location_cursor
+                loop
+                    
+                    htp.print('<section>
+                        <h3>'||location_row.city||'</h3>
+                        <table>
+                            <tr>');
+                                for reservation in (select reservation_id, first, last, room_no, price, arrive_date, depart_date
+                                                    from customers, rooms, reservations, locations
+                                                    where customers.customer_id = reservations.customer_id 
+                                                        AND rooms.room_id = reservations.room_id 
+                                                        AND locations.location_id = rooms.location_id
+                                                        AND locations.location_id = location_row.location_id
+                                                        AND depart_date > sysdate
+                                                    order by arrive_date)
+                                loop
+                                    htp.prn('<td><input type="radio" name="reservation_id_in" value="'||reservation.reservation_id||'"></td>');
+                                    htp.prn('<td>'||reservation.first||' '||reservation.last||'</td>');
+                                    htp.prn('<td>'||reservation.room_no||'</td>');
+                                    htp.prn('<td>'||reservation.price||'</td>');
+                                    htp.prn('<td>'||reservation.arrive_date||'</td>');
+                                    htp.prn('<td>'||reservation.depart_date||'</td>');
+                                end loop;
+                            htp.print('</tr>
+                        </table>
+                    </section>');
+
+                end loop;
+                
+                <input type="submit" value="Submit" name="submit" style="font-size: 20px; background-color:darkgrey; margin-left: 47%;">
+                <p><a href="editReservation.html">Temp link to next page.</a></p>
+            </form>
+        </body>
+    </html>');
+end;
