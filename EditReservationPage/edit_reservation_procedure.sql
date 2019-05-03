62c3d371a76f2bcebf2d5d25a142a65bea55cf7d
Colin Gillette <cgillette@mfncuso.net>
2019-05-03 13:44:20 -0500

    create edit_reservation procedure

--- EditReservationPage/edit_reservation_procedure.sql
+++ EditReservationPage/edit_reservation_procedure.sql
@@ -12,21 +12,27 @@ create or replace procedure edit_reservation
     adults_text reservations.adults%type;
     kids_text reservations.kids%type;
     city_text locations.city%type;
+    room_id_text rooms.room_id%type;
     room_no_text rooms.room_no%type;
     price_text rooms.price%type;
+    max_adults rooms.fits_no_adults%type;
+    max_kids rooms.fits_no_kids%type;
 begin
-    select c.first, c.last, c.card_no, c.card_type, re.arrive_date, re.depart_date, re.adults, re.kids, l.city, ro.room_no, to_char(ro.price, '$999.99')
-    into first_text, last_text, card_no_text, card_type_text, arrive_date_text, depart_date_text, adults_text, kids_text, city_text, room_no_text, price_text
+    select c.first, c.last, c.card_no, c.card_type, re.arrive_date, re.depart_date, re.adults, re.kids, l.city, ro.room_id, ro.room_no, to_char(ro.price, '$999.99')
+    into first_text, last_text, card_no_text, card_type_text, arrive_date_text, depart_date_text, adults_text, kids_text, city_text, room_id_text, room_no_text, price_text
     from customers c, locations l, rooms ro, reservations re
     where c.customer_id = re.customer_id
         and ro.room_id = re.room_id
         and l.location_id = ro.location_id
         and re.reservation_id = reservation_id_in;
 
+    max_adults := max_people('adults', room_id_text);
+    max_kids := max_people('kids', room_id_text);
+
     dbms_output.put_line(first_text || ' ' || last_text ||': Reservation Number ' || reservation_id_in);
     dbms_output.put_line(city_text || ' location, Room: ' || room_no_text || ' - ' || price_text || ' per night.');
     dbms_output.put_line('Credit Card: ' || card_type_text || ' ' || card_no_text);
     dbms_output.put_line('Dates of Stay: ' || arrive_date_text || ' - ' || depart_date_text);
-    dbms_output.put_line('Adults: ' || adults_text);
-    dbms_output.put_line('Kids: ' || kids_text);
+    dbms_output.put_line('Adults: ' || adults_text || ' of ' || max_adults || ' maximum.');
+    dbms_output.put_line('Kids: ' || kids_text || ' of ' || max_kids || ' maximum.');
 end;
