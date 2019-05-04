ab55acb100d89146fd948d37da9badc6348f82d0
Colin Gillette <colingillette35@gmail.com>
2019-05-04 10:30:38 -0500

    initial debug attempt of new_reservation

--- CreateReservationResponsePage/create_reservation_response_toolkit.sql
+++ CreateReservationResponsePage/create_reservation_response_toolkit.sql
@@ -1,18 +1,20 @@
 create or replace procedure new_reservation 
-    (first_name_in customers.first%type,
-    last_name_in customers.last%type,
-    adults_in rooms.fits_no_adults%type,
-    children_in rooms.fits_no_kids%type,
-    arrival_in reservations.arrive_date%type,
-    depart_in reservations.depart_date%type,
-    city_in locations.city%type,
-    card_number_in customers.card_no%type,
-    card_company_name_in customers.card_type%type,
-    room_id_in rooms.room_id%type,
+    (first_name_in varchar2,
+    last_name_in varchar2,
+    adults_in number,
+    children_in number,
+    arrival_in varchar2,
+    depart_in varchar2,
+    city_in varchar2,
+    card_number_in varchar2,
+    card_company_name_in varchar2,
+    room_id_in number,
     submit_button_in varchar2) IS
         location_id_text locations.location_id%type;
         customer_id_text customers.customer_id%type;
         room_no_text rooms.room_no%type;
+        arrival_in_date reservations.arrive_date%type;
+        depart_in_date reservations.depart_date%type;
 begin 
     customer_id_text := get_customer_id(first_name_in, last_name_in, card_number_in, card_company_name_in);
 
@@ -20,6 +22,9 @@ begin
     
     insert into reservations (reservation_id, room_id, customer_id, arrive_date, depart_date, adults, kids)
         values (reservation_id_seq.nextval, room_id_in, customer_id_text, arrival_in, depart_in, adults_in, children_in);
+    
+    arrival_in_date := to_date(arrival_in, 'yyyy/mm/dd');
+    depart_in_date := to_date(depart_in, 'yyyy/mm/dd');
     
     htp.print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
     <html>
