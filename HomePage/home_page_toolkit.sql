817dbdcc2f68ea50ef0eb8d37452775a7079f1de
Colin Gillette <colingillette35@gmail.com>
2019-05-05 08:20:48 -0500

    added a home page

---
+++ HomePage/home_page_toolkit.sql
@@ -0,0 +1,21 @@
+create or replace procedure home_page is
+begin
+  htp.print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+  <html>
+      <head>
+          <title>Hotel Reservation System</title>
+      </head>
+      <div style="text-align: center">
+          <head><a href="create_new_reservation">Create Reservation</a></head>
+          <head><a href="show_current_reservations">Administration</a></head>
+      <hr></div>
+  <title>Example Hotel Company</title>
+  <body>
+      <div style="text-align: center;">
+          <h1>Example Hotel Company</h1>
+          <p><h2>This will contain detail about the company.</h2>
+          </p>
+      </div>
+  </body>
+  </html>');
+end;
